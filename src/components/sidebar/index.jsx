ba2fa68173c613d828117568461101a435e87418
viitones <vitonesmartins@gmail.com>
2024-07-16 15:24:19 -0300

    responsively

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -11,7 +11,7 @@ return (
       src={"https://images.unsplash.com/photo-1526374870839-e155464bb9b2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="foto de perfil do victor" />
 
     <div>
-      <Avatar src="https://github.com/maykbrito.png"/>
+      <Avatar src="https://github.com/rocketseat-education.png"/>
       <strong>Victor Hugo</strong>
       <span>Web Developer</span>
     </div>
