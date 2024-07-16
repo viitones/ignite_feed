80b00333ca7f9c40ad9118a69b0da67edb53b817
viitones <vitonesmartins@gmail.com>
2024-07-16 15:47:11 -0300

    resolve deploy bug pt.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import styles from "./app.module.css"
 
-import { Header } from "./components/Header/index.jsx";
+// import { Header } from "./components/Header/index.jsx";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
@@ -9,7 +9,7 @@ export function App() {
 
   return (
     <div >
-      <Header />
+      {/* <Header /> */}
 
       <div className="wrapper">
         <div className={styles.wrapper}>
