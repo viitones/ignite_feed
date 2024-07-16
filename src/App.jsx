08ea7009a064adc5332e03d76139d3ef96ed480f
viitones <vitonesmartins@gmail.com>
2024-07-16 15:42:32 -0300

    resolve deploy bug pt.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import styles from "./app.module.css"
 
-import { Header } from "./components/Header/index";
+import { Header } from "./components/Header/index.jsx";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
